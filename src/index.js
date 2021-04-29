952d75864be9399ab30d49ee3bcb136743390d54
bashforger <bash.forger@gmail.com>
2021-04-29 21:04:53 +0500

    Added basic header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import header from './header';
 import codefactory from './codefactory';
 
+header();
   // codefactory( _type, {_attr}, _content, _parent_id )
 codefactory('h1', { class: '' }, 'Hello World!', 'content');
