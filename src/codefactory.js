d7a4471fbf82d15c2702d997917aef0511281c6f
bashforger <bash.forger@gmail.com>
2021-04-29 20:51:08 +0500

    Webpack integration test

---
+++ src/codefactory.js
@@ -0,0 +1,9 @@
+export const codefactory = (_type, _attr, _content, _parentId) => {
+  const node = document.createElement(_type);
+  node.innerText = _content;
+  Object.keys(_attr).forEach((attr) => node.setAttribute(attr, _attr[attr]));
+  document.getElementById(_parentId).appendChild(node);
+  return node;
+};
+
+export default codefactory;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import codefactory from './codefactory';
+
+  // codefactory( _type, {_attr}, _content, _parent_id )
+codefactory('h1', { class: '' }, 'Hello World!', 'content');
