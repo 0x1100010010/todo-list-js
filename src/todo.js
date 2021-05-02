7d4c0390df113e6af4e11f33749bde1e3c554049
bashforger <bash.forger@gmail.com>
2021-05-03 00:58:53 +0500

    Added butten events

--- src/todo.js
+++ src/todo.js
@@ -5,11 +5,11 @@ export const todo = () => {
   codefactory('div', { class: 'border border-gray-200 p-6 rounded-lg flex flex-col', id: 'todo-flex-container' }, '', 'todo-flex-container-size');
   
   codefactory('h1', { class: 'text-4xl text-gray-600 pb-4 mb-4 border-b border-gray-200 leading-none self-center' }, 'Tasks', 'todo-flex-container');
-  codefactory('button', { class: 'flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded', id: 'todo-add-button' }, '', 'todo-flex-container');
+  codefactory('button', { class: 'flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded', id: 'todo-add-button' }, '', 'todo-flex-container').addEventListener('click', () => { taskForm.classList.toggle('hidden') });;
   codefactory('i', { class: "fas fa-tasks mr-3" }, '', 'todo-add-button');
   codefactory('span', { class: "" }, 'Add Task', 'todo-add-button');
   codefactory('i', { class: "fas fa-plus-square ml-auto" }, '', 'todo-add-button');
-  codefactory('h2', { class: 'text-lg text-gray-900 font-medium title-font mb-2' }, 'Shooting Stars', 'todo-flex-container');
+  const taskForm = codefactory('div', { class: 'border border-gray-200 my-3 p-6 rounded-lg flex flex-col hidden', id: 'project-form' }, 'Shooting Stars', 'todo-flex-container');
 }
 
 // export default project;
